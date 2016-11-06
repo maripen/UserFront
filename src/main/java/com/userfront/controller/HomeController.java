09f6c1a43a61b7a7946e5129adf8dddcebe1c86a
Tamas Nemeth <maripen@gmail.com>
2016-11-06 18:12:29 +0100

    homepage added

---
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.userfront.controller;
+
+/**
+ * Created by maripen on 2016. 11. 06..
+ */
+public class HomeController {
+}
---
+++ src/main/java/com/userfront/domain/SavingsTransaction.java
@@ -0,0 +1,7 @@
+package com.userfront.domain;
+
+/**
+ * Created by maripen on 2016. 11. 06..
+ */
+public class SavingsTransaction {
+}
